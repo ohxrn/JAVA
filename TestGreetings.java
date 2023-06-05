c78a2224d96ae1ad3fb8f966d779843784566099
Jack Ohrn <jackohrn1@gmail.com>
2023-06-05 18:12:38 -0400

    Add files via upload

---
+++ TestGreetings.java
@@ -0,0 +1,16 @@
+public class TestGreetings {
+
+    public static void main(String[] args) {
+
+        // To use the greetings methods
+        // create a variable of type Greetings
+
+        Greetings greeterApp = new Greetings();
+
+        // Use the variable to access Greetings methods
+        // using dot notation.
+
+        String dateMessage = greeterApp.getCurrentDate();
+        System.out.println(dateMessage);
+    }
+}
